ba56de0ba86aece1f11e533c977da0bff602a361
EduardoSCosta <eduardoscaraujo@gmail.com>
2022-10-28 17:56:53 -0300

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Pomodoro from "./Pomodoro";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Pomodoro />
+  </React.StrictMode>
+);
